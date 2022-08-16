453e2674b103afb3cba4bd82c69750bd67a39ff2
3b8ny <ahmedabdelghny5@gmail.com>
2022-08-16 23:36:12 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+
+var copaya = ''
+
+for (var i = 0; i < 21; i += 1) {
+    copaya += `<div class="col-md-3">
+    <div>
+      <img class="w-100" src="images/client-5.png" alt="">
+      <h2>amira</h2>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+</div>`;
+
+}
+// document.getElementById('demoo').innerHTML = copaya ;
+}
